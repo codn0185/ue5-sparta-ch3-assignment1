6f46e9588f8b03414ae763865c932ba01148a976
김채우 <codn0185@gmail.com>
2026-08-13 20:08:34 +0900

    RotatingActor 블루프린트 생성

--- Source/SpartaCH3Assignment1/Private/RotatingActor.cpp
+++ Source/SpartaCH3Assignment1/Private/RotatingActor.cpp
@@ -2,25 +2,25 @@
 
 ARotatingActor::ARotatingActor()
 {
-    PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = true;
 
-    SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
-    SetRootComponent(SceneRoot);
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	SetRootComponent(SceneRoot);
 
-    StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
-    StaticMeshComp->SetupAttachment(SceneRoot);
+	StaticMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("StaticMesh"));
+	StaticMeshComp->SetupAttachment(SceneRoot);
 
-    RotatingSpeedYaw = 15.0f;
+	RotatingSpeedYaw = 15.0f;
 }
 
 void ARotatingActor::BeginPlay()
 {
-    Super::BeginPlay();
+	Super::BeginPlay();
 }
 
 void ARotatingActor::Tick(float DeltaTime)
 {
-    Super::Tick(DeltaTime);
+	Super::Tick(DeltaTime);
 
-    AddActorLocalRotation(FRotator(0.0f, RotatingSpeedYaw * DeltaTime, 0.0f));
+	AddActorLocalRotation(FRotator(0.0f, RotatingSpeedYaw * DeltaTime, 0.0f));
 }
--- Source/SpartaCH3Assignment1/Public/RotatingActor.h
+++ Source/SpartaCH3Assignment1/Public/RotatingActor.h
@@ -1,28 +1,30 @@
 #pragma once
 
 #include "CoreMinimal.h"
+
 #include "GameFramework/Actor.h"
+
 #include "RotatingActor.generated.h"
 
 UCLASS()
 class SPARTACH3ASSIGNMENT1_API ARotatingActor : public AActor
 {
-    GENERATED_BODY()
+	GENERATED_BODY()
 
   public:
-    ARotatingActor();
+	ARotatingActor();
 
   protected:
-    // Components
-    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
-    USceneComponent* SceneRoot;  // 루트 컴포넌트
-    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
-    UStaticMeshComponent* StaticMeshComp;  // 스태틱 메시 컴포넌트
+	// Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USceneComponent* SceneRoot;  // 루트 컴포넌트
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+	UStaticMeshComponent* StaticMeshComp;  // 스태틱 메시 컴포넌트
 
-    // Properties
-    UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Properties")
-    float RotatingSpeedYaw;  // z축 기준 회전 속도 (초 당 회전 각도)
+	// Properties
+	UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Properties")
+	float RotatingSpeedYaw;  // z축 기준 회전 속도 (초 당 회전 각도)
 
-    virtual void BeginPlay() override;
-    virtual void Tick(float DeltaTime) override;
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
 };
