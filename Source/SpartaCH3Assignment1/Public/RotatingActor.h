233443745bb4bcccb169dca41d72a6791c7cb3a2
김채우 <codn0185@gmail.com>
2026-08-13 15:02:48 +0900

    RotatingActor C++ 파일 작성

--- Source/SpartaCH3Assignment1/Public/RotatingActor.h
+++ Source/SpartaCH3Assignment1/Public/RotatingActor.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -9,18 +7,22 @@
 UCLASS()
 class SPARTACH3ASSIGNMENT1_API ARotatingActor : public AActor
 {
-	GENERATED_BODY()
-	
-public:	
-	// Sets default values for this actor's properties
-	ARotatingActor();
+    GENERATED_BODY()
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+  public:
+    ARotatingActor();
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+  protected:
+    // Components
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+    USceneComponent* SceneRoot;  // 루트 컴포넌트
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Components")
+    UStaticMeshComponent* StaticMeshComp;  // 스태틱 메시 컴포넌트
 
+    // Properties
+    UPROPERTY(EditInstanceOnly, BlueprintReadOnly, Category = "Properties")
+    float RotatingSpeedYaw;  // z축 기준 회전 속도 (초 당 회전 각도)
+
+    virtual void BeginPlay() override;
+    virtual void Tick(float DeltaTime) override;
 };
